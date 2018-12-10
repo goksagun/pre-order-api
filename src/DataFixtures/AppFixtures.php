b079e7dfa48e7450a2f52bbd14f75590cce843c2
Burak Bolat <burak.bolat@metglobal.com>
2018-12-10 13:34:04 +0300

    add cart fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Cart;
+use App\Entity\CartProduct;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -9,13 +11,25 @@ use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
-    public const NUM_ITEMS = 99;
+    public const PRODUCT_REFERENCE_PREFIX = 'product';
+    public const PRODUCT_NUM_ITEMS = 99;
+    public const CART_REFERENCE_PREFIX = 'cart';
+    public const CART_NUM_ITEMS = 9;
+    public const CART_PRODUCT_REFERENCE_PREFIX = 'cart_product';
+    public const CART_PRODUCT_NUM_ITEMS = 33;
 
     public function load(ObjectManager $manager)
     {
+        $this->loadProducts($manager);
+        $this->loadCarts($manager);
+        $this->loadCartProducts($manager);
+    }
+
+    private function loadProducts(ObjectManager $manager)
+    {
         $faker = Factory::create();
 
-        for ($i = 0; $i < self::NUM_ITEMS; ++$i) {
+        for ($i = 1; $i <= self::PRODUCT_NUM_ITEMS; ++$i) {
             $product = new Product();
             $product->setSku($faker->ean8);
             $product->setName(ucwords($faker->words($faker->numberBetween(1, 5), true)));
@@ -24,6 +38,66 @@ class AppFixtures extends Fixture
             $product->setDescription($faker->boolean(70) ? $faker->text : null);
 
             $manager->persist($product);
+
+            $this->addReference(sprintf('%s_%d', self::PRODUCT_REFERENCE_PREFIX, $i), $product);
+
+            if ($i % 20 == 0) {
+                $manager->flush();
+                $manager->clear();
+            }
+        }
+
+        $manager->flush();
+    }
+
+    private function loadCarts(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= self::CART_NUM_ITEMS; ++$i) {
+            $cart = new Cart();
+            $cart->setType(Cart::TYPE_CART);
+
+            $manager->persist($cart);
+
+            $this->addReference(sprintf('%s_%d', self::CART_REFERENCE_PREFIX, $i), $cart);
+
+            if ($i % 20 == 0) {
+                $manager->flush();
+                $manager->clear();
+            }
+        }
+
+        $manager->flush();
+    }
+
+    private function loadCartProducts(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        for ($i = 1; $i <= self::CART_PRODUCT_NUM_ITEMS; ++$i) {
+            $cartProduct = new CartProduct();
+            /** @var Cart $cart */
+            $cart = $this->getReference(
+                sprintf('%s_%d', self::CART_REFERENCE_PREFIX, $faker->numberBetween(1, self::CART_NUM_ITEMS))
+            );
+            /** @var Product $product */
+            $product = $this->getReference(
+                sprintf('%s_%d', self::PRODUCT_REFERENCE_PREFIX, $faker->numberBetween(1, self::PRODUCT_NUM_ITEMS))
+            );
+
+            $cartProduct->setProduct($product);
+            $cartProduct->setType(CartProduct::TYPE_CART_ITEM);
+            $cartProduct->setQuantity($faker->numberBetween(1, 9));
+
+            $cart->addItem($cartProduct);
+
+            $manager->persist($cartProduct);
+
+            $this->addReference(sprintf('%s_%d', self::CART_PRODUCT_REFERENCE_PREFIX, $i), $cartProduct);
+
+            if ($i % 20 == 0) {
+                $manager->flush();
+                $manager->clear();
+            }
         }
 
         $manager->flush();
